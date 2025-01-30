56b4123f8a51c71eaf89f349675adcbdf06992d4
basshightech <bfaye4178@gmail.com>
2025-01-30 10:20:59 +0000

    Pending changes exported from your codespace

---
+++ backend/recrutement/src/main/java/unchk/sn/recrutement/controller/helloController.java
@@ -0,0 +1,10 @@
+package unchk.sn.recrutement.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class helloController {
+    
+    @GetMapping(./helloController)
+    public string hello() {
+        return "Hello, World!";
+}
